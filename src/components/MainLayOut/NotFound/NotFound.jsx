8f163bcd55d07618b82ea69fe3599cd9387b528b
yeaminstudent5598 <yeaminstudent5598@gmail.com>
2025-02-12 12:16:10 +0600

    feat: Created a custom 404 error page with home navigation

--- src/components/MainLayOut/NotFound/NotFound.jsx
+++ src/components/MainLayOut/NotFound/NotFound.jsx
@@ -9,9 +9,9 @@ const NotFound = () => {
             className="min-h-screen  flex flex-col items-center justify-center bg-cover bg-center"
             style={{
                 backgroundImage: `url('https://i.ibb.co.com/G3dk6fr/404-background-img.png')`,
-            }}
+            }} 
         >
-            {/* Subject Image */}
+            {/* Subject Image */} 
             <div className="mb-8 pl-20">
                 <img
                     src="https://i.ibb.co.com/cx5LNww/404-img.png"
